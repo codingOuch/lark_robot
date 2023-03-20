48ca10f826988e8edc4e1d0a421959eb1b4119a2
Howard Zhu <haowen.zhu@mihoyo.com>
2023-03-21 04:09:40 +0800

    改下逻辑

--- python/api.py
+++ python/api.py
@@ -26,7 +26,10 @@ class MessageApiClient(object):
         return self._tenant_access_token
 
     def send_text_with_open_id(self, open_id, content):
-        self.send("open_id", open_id, "text", f"你说的是{content}吗")
+        if 'text' in content.keys():
+            self.send("open_id", open_id, "text", {"text": "你说的是%s吗？" % content["text"]})
+        else:
+            self.send("open_id", open_id, "text", content)
 
     def send_chatgpt_answer(self, open_id, question):
         logging.debug(f"OpenID: {open_id}")
